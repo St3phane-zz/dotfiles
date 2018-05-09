ea0437f28488aad06a6f9926072a9648c96a1ac7
St3phane <point.steph@gmail.com>
2018-05-09 14:19:03 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,36 @@
+# dotfiles
+
+## Installation
+
+```
+mkdir -p ${HOME}/code/src/github.com/ViBiOh/
+cd ${HOME}/code/src/github.com/ViBiOh/
+git clone https://github.com/ViBiOh/dotfiles.git
+./dotfiles/install.sh
+./dotfiles/clone.sh
+```
+
+## SSH
+
+### Generate key and deploy key
+
+```bash
+ssh-keygen -t ed25519
+ssh-copy-id -i ${HOME}/.ssh/id_ed25519.pub docker
+```
+
+## Bash
+
+Then change default bash for root
+
+```bash
+sudo -s
+echo /usr/local/bin/bash >> /etc/shells
+chsh -s /usr/local/bin/bash
+```
+
+And also for current user
+
+```bash
+chsh -s /usr/local/bin/bash
+```
---
+++ install.sh
@@ -0,0 +1,92 @@
+#!/usr/bin/env bash
+
+set -e
+
+for file in ${HOME}/dofiles/symlinks/*; do
+  [ -r "${file}" ] && [ -e "${file}" ] && rm -f ${HOME}/.`basename ${file}` && ln -s ${file} ${HOME}/.`basename ${file}`
+done
+
+if [ ! -e "${HOME}/.gnupg/gpg-agent.conf" ]; then
+  echo
+  echo Symlinking gpg-agent conf
+  mkdir -p "${HOME}/.gnupg"
+  ln -s "${HOME}/.gpg-agent.conf" "${HOME}/.gnupg/gpg-agent.conf"
+fi
+
+MAC_OS_SSH_CONFIG=""
+if [ `uname` == 'Darwin' ]; then
+  MAC_OS_SSH_CONFIG="
+    UseKeyChain no"
+fi
+
+if [ `uname` == 'Darwin' ]; then
+  if ! command -v brew > /dev/null 2>&1; then
+    echo Installing brew
+    /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
+    echo
+    echo Installing common brew tools
+    brew install \
+      bash \
+      git \
+      bash-completion \
+      tmux \
+      reattach-to-user-namespace \
+      fswatch \
+      fzf \
+      fd \
+      htop \
+      openssl \
+      gnupg \
+      pass \
+      node \
+      graphviz \
+      pgcli \
+      python
+
+    echo
+    echo Installing curl with right option
+    brew install curl --with-openssl
+    brew link --force curl
+
+    echo
+    echo Configuring FZF
+    /usr/local/opt/fzf/install
+
+    echo
+    echo Configuring Python
+    unlink /usr/local/bin/python
+    ln -s /usr/local/bin/python3 /usr/local/bin/python
+    unlink /usr/local/bin/pip
+    ln -s /usr/local/bin/pip3 /usr/local/bin/pip
+
+    echo
+    echo Configuring pgcli
+    mkdir -p "${HOME}/.config/pgcli"
+    ln -s "${HOME}/.pgclirc" "${HOME}/.config/pgcli/config"
+
+    echo
+    echo Follow instruction in README for configuring bash
+  else
+    echo
+    echo Updating brew packages
+
+    brew update
+    brew upgrade
+    brew cleanup
+  fi
+elif [ `uname` == "Linux" ]; then
+  if [ ! -d "${HOME}/.fzf" ]; then
+    echo
+    echo Installing FZF
+
+    git clone --depth 1 https://github.com/junegunn/fzf.git ${HOME}/.fzf
+    ${HOME}/.fzf/install
+  else
+    echo
+    echo Updating FZF
+
+    cd ${HOME}/.fzf
+    git pull
+  fi
+fi
